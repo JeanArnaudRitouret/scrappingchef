f9283a7d3cf5b83e823f99975624ba319134d379
Jean-Arnaud Ritouret <jean-arnaud.ritouret@cognitedata.com@Jean-Arnauds-MacBook-Pro.local>
2025-07-23 23:31:30 +0200

    update models

---
+++ platform_new/models/__init__.py
@@ -0,0 +1,3 @@
+from .models import Path, Training, Step, Content
+
+__all__ = ['Path', 'Training', 'Step', 'Content']
--- platform_new/models/models.py
+++ platform_new/models/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class BaseModel(models.Model):
-    id = models.CharField(primary_key=True, max_length=100)
+    id = models.CharField(primary_key=True, max_length=500)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
@@ -10,37 +11,37 @@ class BaseModel(models.Model):
 
 
 class Path(BaseModel):
-    platform_id = models.IntegerField(null=False, blank=False)
+    platform_id = models.CharField(max_length=500, null=False, blank=False, unique=True)
     title = models.CharField(max_length=500, null=False, blank=False)
-    progression = models.FloatField(default=0.0, null=False, blank=False)
-    score = models.FloatField(default=0.0, null=False, blank=False)
+    progression = models.FloatField(null=False, blank=False)
+    score = models.FloatField(null=False, blank=False)
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
 
 
 class Training(BaseModel):
-    platform_id = models.IntegerField(null=False, blank=False)
+    platform_id = models.CharField(max_length=500, null=False, blank=False, unique=True)
     path = models.ForeignKey(Path, related_name='trainings', on_delete=models.CASCADE, null=False, blank=False)
     title = models.CharField(max_length=500, null=False, blank=False)
-    progression = models.FloatField(default=0.0, null=False)
-    score = models.FloatField(null=True, default=None)
+    progression = models.FloatField(null=False)
+    score = models.FloatField(null=False)
     type = models.CharField(max_length=500, null=False, blank=False)
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
 
 
 class Step(BaseModel):
-    platform_id = models.IntegerField(null=False, blank=False)
+    platform_id = models.IntegerField(null=False, blank=False, unique=True)
     training = models.ForeignKey(Training, related_name='steps', on_delete=models.CASCADE, null=False, blank=False)
     title = models.CharField(max_length=500, null=False, blank=False)
     type = models.CharField(max_length=500, null=False, blank=False)
-    is_validated = models.BooleanField(default=False, null=False, blank=False)
-    is_blocked = models.BooleanField(default=False, null=False, blank=False)
+    is_validated = models.BooleanField(default=False, null=False, blank=False)  # type: ignore
+    is_blocked = models.BooleanField(default=False, null=False, blank=False)  # type: ignore
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
 
 
 class Content(BaseModel):
@@ -48,5 +49,5 @@ class Content(BaseModel):
     filename = models.CharField(max_length=500, null=False, blank=False)
     type = models.CharField(max_length=500, null=False, blank=False)
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.filename)
